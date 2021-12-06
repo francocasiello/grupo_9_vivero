00fd590a4a6a5e67bdfb26cd8ae73d35a710c731
francocasiello <francocasiello@gmail.com>
2021-12-06 00:24:51 -0300

    session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const createError = require('http-errors');
 const cookieParser = require('cookie-parser');
 const express = require('express');
+const session = require('express-session');
 const logger = require('morgan');
 const path = require('path');
 const methodOverride =  require('method-override'); // Pasar poder usar los métodos PUT y DELETE
@@ -17,6 +18,17 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(methodOverride('_method')); // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE
 
+
+//*****Session ****///
+app.use(session({
+  secret: "Secreto",
+  resave: false,
+  saveUninitialized: false,
+}));
+
+// Middlewares
+const userLoggedMiddleware = require("./src/middlewares/userLoggedMiddleware")
+app.use(userLoggedMiddleware);
 
 const publicPath = path.resolve(__dirname, "./public");
 
--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -8,15 +8,94 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
 
+const {validationResult} = require("express-validator");
+
+const User = require("../models/User")
+
+const bcryptjs = require("bcryptjs");
+
 const usersController = {
-login: (req, res) => { return res.render("login")},
-register: (req, res) => { return res.render("register")},
-processRegister: (req, res) => { return res.send(req.body)},
+login: (req, res) => { 
+    return res.render("login")
+},
 
-//profile: (req, res) => { return res.render("userProfile")},
+loginProcess: (req, res ) => {
+    let userToLogin = User.findByField("email", req.body.email);
+    
+    if(userToLogin) {
+        let isOkThePassword = bcryptjs.compareSync(req.body.password, userToLogin.password)
+        if(isOkThePassword) {
+            delete userToLogin.password;
+            req.session.userLogged = userToLogin;
+            return res.redirect("profile")
+        }
+        return res.render("login", {
+            errors: {
+                password: {
+                    msg: "Contraseña incorrecta"
+                }
+            }
+        })
+    } 
+    
+    return res.render("login", {
+        errors: {
+            email: {
+                msg: "Usuario no encontrado"
+            }
+        }
+    })
+},
 
+register: (req, res) => { 
+    return res.render("register")
+},
+
+processRegister: (req, res) => { 
+    const resultValidation = validationResult(req);
+    //return res.send(resultValidation.errors.length);
+    if (resultValidation.errors.length > 0) {
+        return res.render ("register", {
+            errors: resultValidation.mapped(),
+            oldData: req.body
+       });
+    } 
+
+    let userInDB = User.findByField("email", req.body.email);
+    if (userInDB) {
+        return res.render ("register", {
+            errors: {
+                email: {
+                    msg: "Este email ya esta registrado"
+                }
+            },
+            oldData: req.body
+       });
+    }
+
+    let userToCreate = {
+        ...req.body,
+        password: bcryptjs.hashSync(req.body.password, 10),
+        avatar: req.file.filename
+    }
+
+    let userCreated = User.create(userToCreate);
+    return res.redirect("/user/login")
+},
+
+logout: (req, res) => {
+    req.session.destroy();
+    return res.redirect("/");
+},
+
+profile: (req, res) => { 
+    return res.render("userProfile", {
+        user: req.session.userLogged
+    })
+}
 
 };
+
 
 // Acá exportamos el resultado
 module.exports= usersController
